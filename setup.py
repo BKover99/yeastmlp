13c111b72d5671800058649d79211670f6a77c47
BKover99 <91386576+BKover99@users.noreply.github.com>
2023-06-11 22:05:47 +0100

    add files for example

---
+++ setup.py
@@ -0,0 +1,30 @@
+#this is a setup.py file
+from setuptools import setup, find_packages
+
+
+
+
+VERSION = '0.0.3'
+DESCRIPTION = 'Python package for analysis of Multicellular-like phenotype formation in yeast species'
+LONG_DESCRIPTION = 'Python package for analysis of Multicellular-like phenotype formation in yeast, for more information see https://github.com/BKover99/yeastmlp'
+
+
+setup(
+    name="yeastmlp",
+    version=VERSION,
+    author="Bence Kover",
+    author_email="<kover.bence@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['numpy','scipy', 'scikit-image', 'matplotlib', 'pandas'],
+    keywords=['python', 'yeast', 'biology', 'multicellularity', 'adhesion', 'flocculation'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
+
